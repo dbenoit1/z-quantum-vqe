30d85323e1f0fa4b3d63695a16aab29d1aa15679
David Benoit <d.benoit@hull.ac.uk>
2022-05-06 14:21:16 +0100

    Update HF_circuit.py
    sanity check for rotations

--- src/python/zquantum/vqe/HF_circuit.py
+++ src/python/zquantum/vqe/HF_circuit.py
@@ -56,7 +56,8 @@ class HF_Ansatz(Ansatz):
         for qubit_index in range(self.number_of_qubits):
 
             qubit_parameters = parameters[qubit_index : (qubit_index + 1)]
-            circuit += RY(qubit_parameters[0]*zero)(qubit_index)
+            c€ircuit += RY(qubit_parameters[0]*zero)(qubit_index)
+            circuit += RY(qubit_parameters[0])(qubit_index)
             #circuit += RY(-qubit_parameters[0])(qubit_index)
 
         return circuit
