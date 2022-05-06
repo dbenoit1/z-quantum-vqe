705fdc86a8955db33c03e7b6de11048d95f4ab9e
David Benoit <d.benoit@hull.ac.uk>
2022-05-06 09:31:57 +0100

    Update HF_circuit.py

--- src/python/zquantum/vqe/HF_circuit.py
+++ src/python/zquantum/vqe/HF_circuit.py
@@ -55,8 +55,8 @@ class HF_Ansatz(Ansatz):
         for qubit_index in range(self.number_of_qubits):
 
             #qubit_parameters = parameters[qubit_index : (qubit_index + 1)]
-            angle=parameters[0]        
-            circuit += RY(0*angle)(qubit_index)
+            zero=0.0
+            circuit += RY(zero*parameters[0])(qubit_index)
             #circuit += RY(-qubit_parameters[0])(qubit_index)
 
         return circuit
