a3729dba0fd2be9939d74a7b45c071a48d250a4a
David Benoit <d.benoit@hull.ac.uk>
2022-05-10 17:52:38 +0100

    Update HF_circuit.py
    Removed even number of qubits check to allow for tapered Hamiltonians

--- src/python/zquantum/vqe/HF_circuit.py
+++ src/python/zquantum/vqe/HF_circuit.py
@@ -43,7 +43,7 @@ class HF_Ansatz(Ansatz):
         if number_of_layers <= 0:
             raise ValueError("number_of_layers must be a positive integer")
         super().__init__(number_of_layers)
-        assert number_of_qubits % 2 == 0
+       # assert number_of_qubits % 2 == 0
         self._number_of_qubits = number_of_qubits
         self._nb_occ = nb_occ
 
