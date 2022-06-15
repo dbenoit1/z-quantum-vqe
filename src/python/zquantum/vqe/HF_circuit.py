24e89f9b87193e8b8e5dce6e11373189d6a5e7b3
David Benoit <d.benoit@hull.ac.uk>
2022-06-15 16:06:17 +0100

    Update HF_circuit.py

--- src/python/zquantum/vqe/HF_circuit.py
+++ src/python/zquantum/vqe/HF_circuit.py
@@ -143,7 +143,7 @@ class HF_Ansatz(Ansatz):
         print(circuit.operations)
         for gates in circuit.operations:
             print(gates.qubit_indices)
-            myval,_=gates.qubit_indices
+            myval=gates.qubit_indices
             print(myval,type(myval))
 
         print(circuit.to_unitary())
