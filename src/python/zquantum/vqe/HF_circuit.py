ed6ab73bb44e69dcfa3e2364d5c15bd376880fb8
David Benoit <d.benoit@hull.ac.uk>
2022-06-15 13:19:04 +0100

    Update HF_circuit.py

--- src/python/zquantum/vqe/HF_circuit.py
+++ src/python/zquantum/vqe/HF_circuit.py
@@ -138,6 +138,8 @@ class HF_Ansatz(Ansatz):
             number_of_alpha_electrons=self.nb_occ//2,number_of_beta_electrons=self.nb_occ//2,
             transformation="Jordan-Wigner",
         )
+        
+        print(circuit.to_text_diagram())
 
        # # Hardwired JW HF ansatz instead (previous one has library issues)
        # for i in range(self.nb_occ):
