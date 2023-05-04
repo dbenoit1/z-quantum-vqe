0e541c07957d0db416b1f7ee01d98a498c114699
David Benoit <d.benoit@hull.ac.uk>
2023-05-04 15:51:39 +0100

    Update HEA-rycnot.py

--- src/python/zquantum/vqe/HEA-rycnot.py
+++ src/python/zquantum/vqe/HEA-rycnot.py
@@ -139,6 +139,25 @@ class HEA_RY_CNOT_RY_Ansatz(Ansatz):
             circuit, parameters[0: self.number_of_qubits]
         )
         
+        #Keep track of which qubits are occupied (X) in occupied_qubit_list (0 is unocupied, 1 occupied)
+        occupied_qubit_list=np.zeros(self.number_of_qubits)
+        
+        print(circuit)
+        
+        #This loops over the operations in the circuit and those whould only be Xgates for the occupied qubits
+        for gates in circuit.operations:
+            # extract occupied qubit index
+            myval=gates.qubit_indices[0]
+            # set occupied marker to 1 to represent its state
+            if (occupied_qubit_list[myval]==0):
+                # Here we are just being careful that we dont erase a qubit that was already set
+                # was 0 and now flipped to 1
+                occupied_qubit_list[myval]=1
+                print("X operation on qubit: "+str(myval))
+           
+        print("full occupation map")
+        print(occupied_qubit_list)
+        
         if (self.number_of_layers>0):
             for layer_index in range(self.number_of_layers):
                 circuit += self._build_circuit_layer(
@@ -167,6 +186,13 @@ class HEA_RY_CNOT_RY_Ansatz(Ansatz):
         Returns number of parameters in the layer.
         """
         return self.number_of_qubits 
+    
+    @property
+    def list_of_occupied_qubits(self) -> List[float]:
+        """
+        Returns a list of occupied qubits
+        """
+        return self.occupied_qubit_list 
 
     @property
     def symbols(self) -> List[sympy.Symbol]:
